261564be8f028a1252db304ccc73c34f9d305c8f
Waptech <abdulfataiyakub07@gmail.com>
2023-12-02 12:09:44 +0100

    no chnages

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    setMovies(data.Search); 
+    setMovies(data.Search);
   };
 
   return (
@@ -41,7 +41,7 @@ const App = () => {
         <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
-            ))}
+          ))}
         </div>
       ) : (
         <div className="empty">
